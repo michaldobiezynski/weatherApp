915d47021fd4c530d76d681879227f0a10b68bfd
michaldobiezynski <dobiezynski@gmail.com>
2020-07-24 10:54:09 +0100

    customizing http requests

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const request = require("request");
 
 const url =
-  "http://api.weatherstack.com/current?access_key=fc87f55c77c5849e40978cc051a3cd02&query=New%20York";
+  "http://api.weatherstack.com/current?access_key=fc87f55c77c5849e40978cc051a3cd02&query=New%20York&units=m";
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  // console.log(response.body.current);
+  console.log(
+    response.body.current.weather_descriptions[0] +
+      ". It is " +
+      response.body.current.temperature +
+      " degrees. It feels like " +
+      response.body.current.feelslike +
+      " degrees."
+  );
 });
