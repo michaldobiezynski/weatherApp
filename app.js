27518e4b8c6a145c4bf55b89ffce95e4ac723fa1
michaldobiezynski <dobiezynski@gmail.com>
2020-07-27 20:28:35 +0100

    querrying geodata mapbox api for coords of a place

--- app.js
+++ app.js
@@ -14,3 +14,18 @@ request({ url: url, json: true }, (error, response) => {
       " degrees."
   );
 });
+
+const mapboxUrl =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWljaGFsLWNvbnRpbnV1bSIsImEiOiJjazlsamRydzYwNHduM2hxdXhldDVkb3p3In0.MDQkwULqfGrO6e3NVKlR_Q&limit=1";
+
+request({ url: mapboxUrl, json: true }, (error, response) => {
+  // console.log(response.body.current);
+  console.log(
+    "The lattitude of LA is: " +
+      response.body.features[0].geometry.coordinates[0]
+  );
+  console.log(
+    "The longitude of LA is: " +
+      response.body.features[0].geometry.coordinates[1]
+  );
+});
