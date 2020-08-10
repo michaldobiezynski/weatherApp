d0f319225daf4c41ce94e9163d920e16016d4b60
michaldobiezynski <dobiezynski@gmail.com>
2020-08-10 21:10:45 +0100

    serving HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,22 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Hello express!");
+  res.send("<h1>Weather</h1>");
 });
 
 app.get("/help", (req, res) => {
-  res.send("Help page");
+  res.send([
+    { name: "Michal", age: 26 },
+    { name: "Wika", age: 24 },
+  ]);
 });
 
 app.get("/about", (req, res) => {
-  res.send("About page");
+  res.send("<h1>About</h1>");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Weather page");
+  res.send({ forecast: "25C, feels like 30C", location: "Edinburgh" });
 });
 
 app.listen(3000, () => {
