a0a7c2f845dc6639dde0ef6ddffd050dd9094a6e
michaldobiezynski <dobiezynski@gmail.com>
2020-08-14 09:01:41 +0100

    wiring up the user interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -8,17 +8,29 @@ console.log("Client side javascript file is loaded");
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const messageOne = document.querySelector("#messageOne");
+const messageTwo = document.querySelector("#messageTwo");
+messageOne.textContent = "";
+messageTwo.textContent = "";
 
 weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const location = search.value;
   console.log(location);
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
   fetch(`http://localhost:3000/weather?address=${location}`).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
+          messageOne.textContent = "";
+          messageTwo.textContent = data.error;
+
           console.log(data.error);
         } else {
+          messageTwo.textContent = "";
+          messageOne.textContent = data.location + ". " + data.forecast;
           console.log(data.location);
           console.log(data.forecast);
         }
