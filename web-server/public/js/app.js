4cce3423821718a75531fbc4b68ea15fef40378e
michaldobiezynski <dobiezynski@gmail.com>
2020-08-14 08:47:09 +0100

    creating a search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,15 +6,26 @@ console.log("Client side javascript file is loaded");
 //   });
 // });
 
-fetch("http://localhost:3000/weather?address=Edinburgh").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const location = search.value;
+  console.log(location);
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          console.log(data.location);
+          console.log(data.forecast);
+        }
+      });
     }
-  });
+  );
+  weatherForm.reset();
 });
 
 // const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
